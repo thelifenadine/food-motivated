a37cce08cabd5c4b9064b8f7123c27735c77cebd
Nadine Kinnen <nkinnen@gmail.com>
2021-03-02 15:53:39 -0500

    add new link to footer, update comments, license

--- src/common/components/footer/Contact.js
+++ src/common/components/footer/Contact.js
@@ -3,6 +3,10 @@ import Section from '../layout/Section';
 import { makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
+  pTag: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+  },
   aTag: {
     color: theme.palette.secondary.dark,
     textDecoration: 'none',
@@ -16,7 +20,8 @@ const Contact = () => {
   const classes = useStyles();
   return (
     <Section>
-      Questions/Suggestions? Email: <a className={classes.aTag} href="mailto:rawdogfoodcalculator@gmail.com">rawdogfoodcalculator@gmail.com</a>
+      <p className={classes.pTag}>Questions/Suggestions? Email: <a className={classes.aTag} href="mailto:rawdogfoodcalculator@gmail.com">rawdogfoodcalculator@gmail.com</a></p>
+      <p className={classes.pTag}>New to Raw Feeding? Check out <a className={classes.aTag} href="https://perfectlyrawsome.com/" target="_blank" rel="noopener noreferrer">Perfectly Rawsome</a> for help getting started.</p>
     </Section>
   );
 };
